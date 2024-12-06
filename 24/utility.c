db628c63fc23b9e7a5b7ddbcdbc514786d97d0fd
Agero19 <pmotulko@gmail.com>
2024-12-06 20:05:32 +0100

    Wrote utility module

---
+++ 24/utility.c
@@ -0,0 +1,26 @@
+#include "utility.h"
+
+
+void read_and_store_input(const char *filename, int *list1, int *list2, size_t *count) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    int num1, num2;
+    *count = 0;
+
+    while (fscanf(file, "%d %d", &num1, &num2) == 2) {
+        if (*count >= MAX_NUMBERS) {
+            fprintf(stderr, "Error: Exceeded maximum allowed numbers.\n");
+            fclose(file);
+            exit(EXIT_FAILURE);
+        }
+        list1[*count] = num1;
+        list2[*count] = num2;
+        (*count)++;
+    }
+
+    fclose(file);
+}
---
+++ 24/utility.h
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#define MAX_NUMBERS 1000
+
+// UTILITY FUNCTIONS
+
+//read and store input in separeate lists
+extern void read_and_store_input(const char *filename, int *list1, int *list2, size_t *count);
+
+// SORTING
